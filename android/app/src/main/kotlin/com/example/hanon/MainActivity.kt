6630a9db660f5cd77156247d169f9902a5739c07
Menaga Mohanraj <menagamohan26@gmail.com>
2025-07-25 12:16:43 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/hanon/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.hanon
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
